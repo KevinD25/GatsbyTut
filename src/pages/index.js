d78a47a86b07df2b920b0f738a23d7496e60b1ae
Scazi <kevin-davis@hotmail.com>
2020-07-17 21:59:39 +0200

    Project up to date

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <Header headerText="Hello Gatsby!" />
+      <Header headerText="Hi! I'm building a fake Gatsby site as part of a tutorial!" />
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </Layout>
+  )
 }
